63a0cf1cbd4ddf474a2f98f4329a1b9e782efc26
kazik <antenna@antenna.io>
2016-10-26 10:39:48 +0200

    Add a basic skeleton for the Typeahead component

---
+++ src/Typeahead.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" class="form-control" />
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log('created');
+  },
+  mounted() {
+    console.log('mounted');
+  }
+}
+</script>
+
+<style>
+</style>

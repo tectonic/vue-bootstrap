e2eb565dfbd8a97fc8bb0a92ddbc98e75321fe4f
kazik <antenna@antenna.io>
2016-10-26 12:14:26 +0200

    Build docs with webpack - config file created

---
+++ webpack.docs.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    'docs': path.join(__dirname, 'docs/js/docs.js')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+    ]
+  },
+  devtool: 'sourcemap',
+  debug: true,
+  output: {
+    path: path.join(__dirname, 'docs/js'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[file].map',
+  }
+}

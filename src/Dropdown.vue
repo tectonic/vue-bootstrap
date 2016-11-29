4f726ffdd3eabe1f23c51832c4d19a3fe36a6b5c
kazik <antenna@antenna.io>
2016-11-29 13:25:23 +0100

    Working on Dropdown component

---
+++ src/Dropdown.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="[{ 'open': isOpen }, 'dropdown']">
+    <button
+      class="btn btn-default dropdown-toggle"
+      type="button"
+      :id="id"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="true"
+      @click="toggle"
+      @keyup.esc="isOpen = false">
+      Dropdown <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu" :aria-labelledby="id">
+      <li><a href="#">Action</a></li>
+      <li><a href="#">Another action</a></li>
+      <li><a href="#">Something else here</a></li>
+      <li role="separator" class="divider"></li>
+      <li><a href="#">Separated link</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    toggle () {
+      this.isOpen = (this.isOpen) ? false : true;
+    }
+  },
+  data () {
+    return {
+      isOpen: false,
+    };
+  },
+  mounted () {
+    //
+  }
+};
+</script>
+
+<style>
+</style>

19f7d78d4270e167dc5d671bd5e6558e1f7906c4
kazik <antenna@antenna.io>
2017-09-22 13:29:43 +0200

    Export the modal component from index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Datepicker from './Datepicker.vue';
 import Dropdown from './Dropdown.vue';
+import Modal from './Modal.vue';
 import Popover from './Popover.vue';
 import Typeahead from './Typeahead.vue';
 
 const VueBootstrap = {
   Datepicker,
   Dropdown,
+  Modal,
   Popover,
   Typeahead
 };

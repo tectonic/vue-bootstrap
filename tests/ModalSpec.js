ad2c45f91264e83494ef710a41cddc359c61ec96
kazik <antenna@antenna.io>
2017-09-22 13:04:34 +0200

    Ensure that the modal is visible

--- tests/ModalSpec.js
+++ tests/ModalSpec.js
@@ -24,4 +24,17 @@ describe('Modal', () => {
     expect(vm1.uniqueTitleId()).to.equal('v-bs-modal-title-0');
     expect(vm2.uniqueTitleId()).to.equal('v-bs-modal-title-1');
   });
+
+  it('is visible', () => {
+    const vm = initVM(Modal, {
+      title: 'Hello world!',
+      body: 'Body!'
+    });
+
+    vm.open();
+
+    expect(vm.isVisible).to.be.true;
+    expect(vm.$refs.modal.querySelector('.modal-title').textContent).to.equal('Hello world!');
+    expect(vm.$refs.modal.querySelector('.modal-body').textContent).to.equal('Body!');
+  });
 });

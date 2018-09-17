2b6f7f5dae8f46afefa5540598169e1cbb89f936
kazik <antenna@antenna.io>
2018-09-17 14:08:25 +0200

    Upgrade babel

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
+      'node_modules/@babel/polyfill/dist/polyfill.js',
       'tests/**/*Spec.js'
     ],
 

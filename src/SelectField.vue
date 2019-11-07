0f058bd48a3efb732f9cb6882cd5e07cd83156b8
kazik <antenna@antenna.io>
2019-11-07 13:07:49 +0100

    A workaround for a memory leak when using v-model on a select field

--- src/SelectField.vue
+++ src/SelectField.vue
@@ -1,16 +1,23 @@
 <template>
-  <select :id="id" :name="name" v-model="selected" class="form-control">
+  <select :id="id" :name="name" class="form-control" ref="select">
     <option v-if="emptyOption"></option>
     <template v-for="item in items">
       <template v-if="item.children">
         <optgroup :label="item[valueProperty]">
-          <option v-for="child in item.children" :value="child[idProperty]">
+          <option
+            v-for="child in item.children"
+            :value="child[idProperty]"
+            :selected="child[idProperty] == selected"
+          >
             {{ child[valueProperty] }}
           </option>
         </optgroup>
       </template>
       <template v-else>
-        <option :value="item[idProperty]">
+        <option
+          :value="item[idProperty]"
+          :selected="item[idProperty] == selected"
+        >
           {{ item[valueProperty] }}
         </option>
       </template>
@@ -81,10 +88,22 @@ export default {
     },
     reset () {
       this.selected = null;
+    },
+    onChange (event) {
+      this.select(event.target.value);
     }
   },
   created () {
     this.select(this.value);
+  },
+  mounted () {
+    this.$refs.select.addEventListener('change', this.onChange, false);
+  },
+  beforeDestroy () {
+    this.$refs.select.removeEventListener('change', this.onChange, false);
+
+    // Prevent a memory leak - https://github.com/vuejs/vue/issues/9823
+    this.$el.parentNode.removeChild(this.$refs.select);
   }
 };
 </script>

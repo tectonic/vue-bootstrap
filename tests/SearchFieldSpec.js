8cf473aa2aa834420feded596885e25b0977f40b
kazik <antenna@antenna.io>
2020-01-23 18:12:47 +0100

    Test SearchField component

---
+++ tests/SearchFieldSpec.js
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import SearchField from '../src/SearchField.vue';
+
+describe('SearchField', () => {
+  it('initialises selectedItem', () => {
+    const searchField = shallowMount(SearchField, {
+      propsData: {
+        initialValue: 'Jane',
+        initialId: '1',
+        valueProperty: 'text'
+      }
+    });
+
+    expect(searchField.vm.selectedItem).to.deep.equal({
+      id: '1',
+      text: 'Jane'
+    });
+  });
+
+  it('toggles visibility of the search box', () => {
+    const searchField = shallowMount(SearchField);
+
+    expect(searchField.vm.isOpen).to.equal(false);
+
+    searchField.vm.toggle();
+    expect(searchField.vm.isOpen).to.equal(true);
+  });
+
+  it('handles autocomplete', () => {
+    const searchField = shallowMount(SearchField, {
+      propsData: {
+        initialOptions: [
+          { id: 1, value: 'Jane' },
+          { id: 2, value: 'Jo' },
+          { id: 3, value: 'Jack' },
+        ],
+        initialValue: 'Jane',
+        initialId: '1'
+      }
+    });
+
+    searchField.setData({ isOpen: true });
+
+    searchField.vm.handleAutocomplete({ id: 3, value: 'Jack' });
+
+    expect(searchField.vm.selectedItem).to.deep.equal({ id: 3, value: 'Jack' });
+    expect(searchField.vm.isOpen).to.equal(false);
+    expect(Object.keys(searchField.emitted())).to.include('selected');
+  });
+});
